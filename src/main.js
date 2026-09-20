8211327f9d447411fd6f453e4cc82207f094ffcf
Evandro Conceição <evandrodbz@hotmail.com>
2026-09-20 13:07:06 -0300

    função adicionar pokemon criada

--- src/main.js
+++ src/main.js
@@ -19,5 +19,33 @@ async function buscarPokemon(nomeOuId) {
     }
 }
 buscarPokemon("pikachu").then(function (pokemon) {
-    console.log(pokemon);
+    if (pokemon !== null) {
+        adicionarAoCatalogo(catalogo, pokemon);
+    }
 });
+let catalogo = [];
+function adicionarAoCatalogo(catalogo, pokemon) {
+    const existe = catalogo.some(function (item) {
+        return item.id === pokemon.id;
+    });
+    if (existe) {
+        console.log("[AVISO]", pokemon.nome, "já está no catalogo.");
+    }
+    else {
+        catalogo.push(pokemon);
+        console.log("[OK]", pokemon.nome, "adcionado ao catalogo.");
+    }
+    return catalogo;
+}
+//function listarCatalogo(catalogo: PokemonResumo[]): void {
+//}
+//REMOVER
+//A função deverá:
+//receber o catálogo;
+//receber o ID;
+//verificar se o Pokémon existe;
+//remover o Pokémon, se existir;
+//retornar o catálogo atualizado;
+//exibir mensagem clara no terminal.
+//function removerDoCatalogo(catalogo: PokemonResumo[], id: number): PokemonResumo[]{
+//}
--- src/main.ts
+++ src/main.ts
@@ -29,5 +29,45 @@ async function buscarPokemon(nomeOuId: string | number): Promise<PokemonResumo |
 }
 
 buscarPokemon("pikachu").then(function (pokemon) {
-    console.log(pokemon);
+    
+    if (pokemon !== null) {
+        adicionarAoCatalogo(catalogo, pokemon);
+    }
 });
+
+let catalogo: PokemonResumo[] = [];
+
+function adicionarAoCatalogo(catalogo: PokemonResumo[], pokemon: PokemonResumo): PokemonResumo[] {
+    const existe = catalogo.some(function (item) {
+        return item.id === pokemon.id;
+    });
+
+    if (existe) {
+        console.log("[AVISO]", pokemon.nome, "já está no catalogo.");
+    } else {
+        catalogo.push(pokemon);
+        console.log("[OK]", pokemon.nome, "adcionado ao catalogo.");
+    }
+
+    return catalogo;
+}
+
+
+
+//function listarCatalogo(catalogo: PokemonResumo[]): void {
+
+//}
+
+
+//REMOVER
+//A função deverá:
+//receber o catálogo;
+//receber o ID;
+//verificar se o Pokémon existe;
+//remover o Pokémon, se existir;
+//retornar o catálogo atualizado;
+//exibir mensagem clara no terminal.
+
+//function removerDoCatalogo(catalogo: PokemonResumo[], id: number): PokemonResumo[]{
+
+//}
