2ffc872da272bd1451340003ea51c32b475be4c6
Evandro Conceição <evandrodbz@hotmail.com>
2026-09-26 10:43:42 -0300

    função adicionarPokemon refatorada

--- services/BoxService.ts
+++ services/BoxService.ts
@@ -7,11 +7,37 @@ export async function carregarPokemonBox(): Promise<PokemonResumo[]> {
     return JSON.parse(dados);
 }
 
-export async function salvarPokemonBox(
-    box: PokemonResumo[]
-): Promise<void> {
+export async function salvarPokemonBox(box: PokemonResumo[]): Promise<void> {
 
     const dados = JSON.stringify(box, null, 4);
 
     await writeFile("pc_box.json", dados, "utf-8");
+}
+
+export async function adicionarPokemon(pokemon: PokemonResumo): Promise<void> {
+
+    const box = await carregarPokemonBox();
+
+    const existe = box.some(function (item) {
+        return item.id === pokemon.id;
+    });
+
+    if (existe) {
+        console.log(
+            "[AVISO]",
+            pokemon.nome,
+            "já está na PC Box."
+        );
+        return;
+    }
+
+    box.push(pokemon);
+
+    await salvarPokemonBox(box);
+
+    console.log(
+        "[OK]",
+        pokemon.nome,
+        "adicionado à PC Box."
+    );
 }
